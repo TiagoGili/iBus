03bba7972b6277dd37ee8730190c4ccf60fdb113
jonathan500 <55857909+jonathan500@users.noreply.github.com>
2021-11-25 10:50:08 -0300

    Commit Mapa iBus

--- src/app/pages/home-map/home-map.page.ts
+++ src/app/pages/home-map/home-map.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home-map',
   templateUrl: './home-map.page.html',
   styleUrls: ['./home-map.page.scss'],
 })
-export class HomeMapPage implements OnInit {
+export class HomeMapPage {
+
+  @ViewChild('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
 
   constructor() { }
 
-  ngOnInit() {
+  ionViewWillEnter(){
+    this.exibirMapa();
+  }
+
+  exibirMapa(): void{
+
+    const posicao = new google.maps.LatLng(-22.208066098274095, -49.954102695829);
+    const opcoes = {
+      center: posicao,
+      zoom: 15,
+      disableDefaulUI: false
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
   }
 
 }
